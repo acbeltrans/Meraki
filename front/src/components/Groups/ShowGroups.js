cb97615381740809742fc853da7d4df01ffb740d
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-11-03 12:15:55 -0500

    Fixing deploy

--- front/src/components/Groups/ShowGroups.js
+++ front/src/components/Groups/ShowGroups.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const ShowGroups = () => {
   useEffect(() => {
