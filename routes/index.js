3a56a02d77f455fe83cce2cf92a2855d0d506b90
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-11-03 02:44:49 -0500

    continue working

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-  res.render("index", { title: "Express" });
-});
-
-router.post("/eventbrite", (req, res) => {
-  res.redirect(
-    "https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=5VNMTHNMAGLAEEY5FKM4&redirect_uri=https://dard9811.github.io/Homepage-Daniel-Ramirez/index.html"
-  );
+  res.send("Hello World");
 });
 
 module.exports = router;
