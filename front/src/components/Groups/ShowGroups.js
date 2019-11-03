736043a9de01ea5b76d5a2c8ca9ade4f35ff50d9
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-11-03 12:36:29 -0500

    Connecting to API

--- front/src/components/Groups/ShowGroups.js
+++ front/src/components/Groups/ShowGroups.js
@@ -3,10 +3,18 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const ShowGroups = () => {
   useEffect(() => {
-    fetch(process.env.API_URL).then(data => {
-      console.log("Aqui van", data);
-    });
-  });
+    fetch("https://www.eventbriteapi.com/v3/events/search/", {
+      method: "GET",
+      headers: {
+        Authorization: process.env.PERSONAL_OAUTH_TOKEN,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("Aqui van", data);
+      });
+  }, []);
 
   return (
     <div>
