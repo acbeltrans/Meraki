e33eca58b2c96eadedd898df5eadaa85aa64a642
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-11-04 14:11:52 -0500

    deploy

--- front/src/components/ShowEvents/ShowEvents.js
+++ front/src/components/ShowEvents/ShowEvents.js
@@ -22,7 +22,7 @@ class ShowGroups extends React.Component {
   componentDidMount() {
     fetch("https://www.eventbriteapi.com/v3/events/79481837315/", {
       headers : {
-        Authorization  : process.env.PERSONAL_OAUTH_TOKEN || "Bearer DE5VNBJ7LTY2EMPDM6B4",
+        Authorization  : "Bearer DE5VNBJ7LTY2EMPDM6B4",
         "Content-Type" : "application/json"
       }
     })
@@ -40,7 +40,7 @@ class ShowGroups extends React.Component {
       });
     fetch("https://www.eventbriteapi.com/v3/events/78002049229/", {
       headers : {
-        Authorization  : process.env.PERSONAL_OAUTH_TOKEN || "Bearer DE5VNBJ7LTY2EMPDM6B4",
+        Authorization  : "Bearer DE5VNBJ7LTY2EMPDM6B4",
         "Content-Type" : "application/json"
       }
     })
