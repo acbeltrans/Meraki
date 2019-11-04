4103a261764bcbd9a80814053dcdd8e0062c265e
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-11-04 04:48:53 -0500

    some changes

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home.js";
 import SignHome from "./components/SignHome/SignHome.js";
-import CreateGroups from "./components/Groups/CreateGroup.js";
 import ShowGroups from "./components/Groups/ShowGroups.js";
 import "./App.css";
 
@@ -12,8 +11,7 @@ function App() {
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/Home" component={SignHome} />
-        <Route path="/CreateGroup" component={CreateGroups} />
-        <Route path="/ShowGroups" component={ShowGroups} />
+        <Route exact path="/ShowGroups" component={ShowGroups} />
       </div>
     </Router>
   );
