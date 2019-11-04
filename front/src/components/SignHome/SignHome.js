0d86f7d3556e1cec1c117a74ca8a34aef25b32fc
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-11-04 05:42:34 -0500

    deplying fixes

--- front/src/components/SignHome/SignHome.js
+++ front/src/components/SignHome/SignHome.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./SignHome.css";
 import SignNavbar from "../SignNavBar/SignNavBar.js";
 
