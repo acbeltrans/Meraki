b6e2c98dc44e4188134076b3dea6fea9b1ef9bdc
Andrea <andrea>
2019-12-07 12:02:21 -0500

    run back works

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var bodyParser = require("body-parser");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var usersRouter = require("./routes/users");
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,19 +16,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "./front/build")));
+app.use(express.static(path.join(__dirname, "front/build")));
 
 app.use("/", indexRouter);
-app.use(
-  "/users",
-  function(req, res, next) {
-    req.config = {
-      usersDB : { users }
-    };
-    next();
-  },
-  usersRouter
-);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const MyMongoLib = require("../MyMongoLib.js");
-
+const MyMongoLib = require("../MyMongoLib");
 const myMongoLib = MyMongoLib();
+
+/* GET home page. */
+router.get("/", function(req, res) {
+  res.render("index", { title: "Express" });
+});
 
 /* Get Events */
 
 router.get("/events", function(req, res) {
-  myMongoLib.getEvents().then((doc) => res.send(doc)).catch((err) => res.send(err));
+  myMongoLib
+    .getEvents()
+    .then(doc => res.send(doc))
+    .catch(err => res.send(err));
 });
 
 /* Post Event */
@@ -19,12 +26,15 @@ router.post("/postEvent", (req, res) => {
   let image = req.body.image;
 
   let newEvent = {
-    name        : name,
-    description : description,
-    image       : image
+    name: name,
+    description: description,
+    image: image
   };
 
-  myMongoLib.postEvent(newEvent).then((doc) => res.send(doc)).catch((err) => res.send(err));
+  myMongoLib
+    .postEvent(newEvent)
+    .then(doc => res.send(doc))
+    .catch(err => res.send(err));
 });
 
 module.exports = router;
