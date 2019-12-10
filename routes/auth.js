b2a8b7c301c1afc5a181e8ea5e640ca0b4445fd3
Andrea <ac.beltrans@uniandes.edu.co>
2019-12-10 09:00:39 -0500

    deploy

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const frontURL = "https://meraki-web-app.herokuapp.com/Login" || "http://localhost:3000";
+const frontURL = "/Login" || "http://localhost:3000";
 
 /*Auth login*/
 router.get("/signup", function(req, res) {
